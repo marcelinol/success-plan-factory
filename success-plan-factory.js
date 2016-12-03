5e68b965a1d36bd413630560a2ec5d32b8a61a09
marcelinol <marcelinolucianom@gmail.com>
2016-12-03 04:33:21 -0200

    I'm trying to make it work in the worst way I could. Tomorrow I will take a look

--- success-plan-factory.js
+++ success-plan-factory.js
@@ -5,7 +5,13 @@ var SuccessPlanFactory = (function($) {
       $('.collapse.in').collapse('hide');
       addAction();
     });
-    $('#generate-success-plan').on('click', generateYAML);
+    $('#generate-success-plan').on('click', function(e) {
+      e.preventDefault();
+      var data = $('form').serializeObject();
+      generateYAML(data, '#yaml-pt');
+      generateTranslatedYAML('en');
+      generateTranslatedYAML('es');
+    });
   };
 
   var addAction = function() {
@@ -28,9 +34,45 @@ var SuccessPlanFactory = (function($) {
     return yaml;
   };
 
-  var generateYAML = function(e) {
-    e.preventDefault();
-    var data = $('form').serializeObject();
+  var toTranslate = [
+    'success-plan-title',
+    'success-plan-description',
+    'action-title',
+    'action-description',
+    'action-know-more',
+    'action-know-more-cta'
+  ];
+
+
+  var generateTranslatedYAML = function(language) {
+    var formData = $('form').serializeObject();
+    var texts = [];
+    $.each(toTranslate, function(index, value) {
+      var array = formData[value];
+      for(var x = 0; x < array.length; x++) {
+        array[x] = value + 'Ω ' + array[x]
+      };
+      texts = texts.concat(array);
+    });
+    Translator.translate('pt-' + language, texts, function(data) {
+      $.each(toTranslate, function(index, value) {
+        formData[value] = '';
+      });
+      $.each(data.text, function(index, text) {
+        var info = text.split('Ω ')[0];
+        var translatedText = text.split('Ω ')[1];
+        if(formData[info] === '') {
+          formData[info] = translatedText;
+        } else {
+          formData[info] = [formData[info]].concat([translatedText])
+        }
+      });
+
+      generateYAML(formData, '#yaml-' + language)
+    });
+  };
+
+  var generateYAML = function(data, componentId) {
     var yaml = data['success-plan-key'] + ': ' + data["success-plan-title"] + '\n' +
                data['success-plan-key'] + '_description: ' + data['success-plan-description'] + '\n';
     var actionsLength = data['action-title'].length
@@ -39,7 +81,7 @@ var SuccessPlanFactory = (function($) {
               '  ' + data['action-key'][i] + '_description: ' + data['action-description'][i] + '\n';
       yaml = addSuffixedValues(data, i, yaml)
     }
-    $('#yaml').text(yaml);
+    $(componentId).text(yaml);
     window.scrollTo(0,document.body.scrollHeight);
   }
 
